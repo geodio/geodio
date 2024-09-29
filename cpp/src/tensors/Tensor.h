47d37ab44963c6a4759cbba3036648ba6a695d9d
rares polenciuc <polenciucrares@gmail.com>
2024-09-29 16:44:26 +0200

    license_cpp

--- cpp/src/tensors/Tensor.h
+++ cpp/src/tensors/Tensor.h
@@ -1,4 +1,18 @@
-// Tensor.h
+//    Geodio Neural Network Framework - A lightweight neural network library focused on custom architecture design and optimization.
+//    Copyright (C) Copyright (C) 2024 Geodio (created by Rareș Polenciuc)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef GEODIO_TENSOR_H
 #define GEODIO_TENSOR_H
